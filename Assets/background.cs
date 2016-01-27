fc51e96a69cb3b2f5991e5b72b2e0488ee9cd8bb
Avneet <avneet_KochharEmail>
2016-01-27 08:44:18 -0500

    added player

---
+++ Assets/background.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background : MonoBehaviour {
+
+	// PUBLIC INSTANCE VARIABLES
+	public float speed ;
+
+	//PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		// Make a reference with the Transform Component
+		this._transform = gameObject.GetComponent<Transform> ();
+
+		// Reset the Ocean Sprite to the Top
+		this.Reset ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this._currentPosition = this._transform.position;
+		this._currentPosition -= new Vector2(this.speed,0);
+		this._transform.position = this._currentPosition;
+
+		if (this._currentPosition.x <= -1700) {
+			this.Reset ();
+		}
+
+
+
+
+
+	}
+
+	public void Reset() {
+		this._transform.position = new Vector2 (40f,0);
+	}
+}
