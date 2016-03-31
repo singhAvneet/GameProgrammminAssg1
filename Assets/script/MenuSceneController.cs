c6c1e7c6401d4fbd05fcab9873fcd7bde80db203
Francid <splinzy@yahoo.com>
2016-03-31 19:20:19 -0400

    Added the Menu Scene

---
+++ Assets/script/MenuSceneController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Load Instruction Scene
+	public void InstructionButtonClick(){
+		SceneManager.LoadScene("Instruction");
+	}
+
+	//Load Level 1 Game Scene
+	public void StartGameButtonClick(){
+		SceneManager.LoadScene("main");
+	}
+
+	//Quit Game
+	public void QuitButtonClick(){
+		Application.Quit ();
+	}
+
+	//Load Settings Scene
+	public void SettingsButtonClick(){
+		SceneManager.LoadScene("Settings");
+	}
+
+}
